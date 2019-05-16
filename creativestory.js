513c4d8622eafa8170669b6c91d70bdd71df9ede
xinyashan <50379822+xinyashan@users.noreply.github.com>
2019-05-16 14:57:02 -0500

    Add files via upload

---
+++ creativestory.js
@@ -0,0 +1,43 @@
+var food = 'sandwhiches';
+
+if(food === 'snacks') { console.log('Has bonus effects' + food); }
+
+else if (food === 'foodie') { console.log('This, has powers' + food); }
+
+else { console.log('Once upon a time there is a item called potions. Potions are magical items that has capable healing powers.'); }
+
+
+
+var magic = 'color';
+
+if(magic=== 'and the most magical ones that ever exisited') { console.log('It, can be the greatest item you will ever come across ' + magic); }
+
+else if (magic === 'there is goo inside') { console.log('it, is great ' + magic); }
+
+else { console.log('If its mixed then the color can be changed!'); }
+
+
+var magicalstuff = 'item';
+
+if(magicalstuff === 'item') { console.log('It, can be the greatest item you will ever come across ' + magicalstuff); }
+
+else if (magicalstuff === 'there is goo inside') { console.log('it, is shaped round ' + magicalstuff); }
+
+else { console.log('The colors can be changed in various ways depending on the color palette'); }
+
+
+
+var mystery = 'ways that it can be used.';
+
+if(mystery === 'ways that it can be used') { console.log('Not only does it come in variety,' + mystery); }
+
+else if (mystery === 'mystery to it') { console.log('the, various things ' + mystery); }
+
+else { console.log('This is a mini story of why potions are so awesome. First of all they do not just have the power to heal but they have other effects as well. Potions come in so handy for various things ' +mystery); }
+
+
+
+
+
+
+
